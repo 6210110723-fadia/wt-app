a21b972a4f3c712c402acfacadef02f1cfe94b15
6210110723-fadia <fadia.wl617@gmail.com>
2023-08-07 10:48:02 +0700

    section 4

---
+++ Components/Weather.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Text, ImageBackground, StyleSheet } from "react-native";
+import Forecast from "./Forecast";
+
+export default function Weather(props) {
+    const [forecastInfo, setForecastInfo] = useState({
+        main: '-',
+        description: '-',
+        temp: 0
+    })
+
+    return (
+        <ImageBackground source={require('../bg.jpg')} style={styles.backdrop}>
+            <Text>Zip Code</Text>
+            <Text>{props.zipCode}</Text>
+            <Forecast {...forecastInfo} />
+        </ImageBackground>
+    )
+}
+
+const styles = StyleSheet.create({
+    backdrop: {
+        alignItems: 'column',
+        justifyContent: 'center',
+        width: '100%',
+        height: '100%'
+    }
+})
